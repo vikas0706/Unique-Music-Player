38514d3fb93f974d01d144e10c5954f304d8a4f2
vikas0706 <vikas070696@gmail.com>
2017-01-17 20:46:03 +0530

    Changes in lyrics fetching

--- app/src/main/java/player/music/com/unique/NavigationDrawerFragment.java
+++ app/src/main/java/player/music/com/unique/NavigationDrawerFragment.java
@@ -232,6 +232,7 @@ public class NavigationDrawerFragment extends Fragment implements NavigationDraw
         } catch (ClassCastException e) {
             throw new ClassCastException("Activity must implement NavigationDrawerCallbacks.");
         }
+        
     }
 
     @Override
